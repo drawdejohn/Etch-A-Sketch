42ed6231e53aad9c6000304c8f31eab1bbc80b47
Edward John Felicia <edwardjohnfelicia@gmail.com>
2023-02-17 22:57:56 +0800

    Add a hover effect that changes the color of the square divs

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const container = document.getElementById("container");
 
+// Create a 16x16 square grid and append them to the container
 for (let i = 0; i < 16 * 16; i++) {
 	const square = document.createElement("div");
 	square.classList.add("square");
 	container.appendChild(square);
 }
+
+// Add a hover event listener to each square
+const squares = document.querySelectorAll('.square');
+squares.forEach(square => {
+  square.addEventListener('mouseover', () => {
+    square.style.backgroundColor = 'black';
+  });
+});
