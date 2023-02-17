36fc2c795be1cb94926ebdb6394f3d60dce0e83b
Edward John Felicia <edwardjohnfelicia@gmail.com>
2023-02-17 22:48:19 +0800

    Modify the javascript and add some media queries to support smaller device

--- index.js
+++ index.js
@@ -1,13 +1,7 @@
 const container = document.getElementById("container");
 
-function makeRows(rows, cols) {
-  container.style.setProperty('--grid-rows', rows);
-  container.style.setProperty('--grid-cols', cols);
-  for (c = 0; c < (rows * cols); c++) {
-    let cell = document.createElement("div");
-    cell.innerText = (c + 1);
-    container.appendChild(cell).className = "grid-item";
-  };
-};
-
-makeRows(16, 16);
+for (let i = 0; i < 16 * 16; i++) {
+	const square = document.createElement("div");
+	square.classList.add("square");
+	container.appendChild(square);
+}
