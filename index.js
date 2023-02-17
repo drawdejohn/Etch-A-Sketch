c6ed38fd2192b872192ba129759226329f5d9499
Edward John Felicia <edwardjohnfelicia@gmail.com>
2023-02-17 23:55:46 +0800

    Add the function that prompts the user to enter a new grid size when the new grid button is pressed and Merge the hover function to the createGrid function

--- index.js
+++ index.js
@@ -8,16 +8,21 @@ function createGrid(numSquares) {
   for (let i = 0; i < numSquares ** 2; i++) {
     const square = document.createElement("div");
     square.classList.add("square");
+    square.addEventListener('mouseover', () => {
+      square.style.backgroundColor = 'black';
+    });
     container.appendChild(square);
   }
 }
 createGrid(16);
 
-// Add a hover event listener to each square
-const squares = document.querySelectorAll('.square');
-squares.forEach(square => {
-  square.addEventListener('mouseover', () => {
-    square.style.backgroundColor = 'black';
-  });
+const newGridBtn = document.getElementById("new-grid-btn");
+newGridBtn.addEventListener("click", () => {
+  let numSquares = prompt("Enter the number of squares per side (max 100):");
+  numSquares = parseInt(numSquares);
+  if (!isNaN(numSquares) && numSquares <= 100) {
+    createGrid(numSquares);
+  } else {
+    alert("Please enter a valid number between 1 and 100.");
+  }
 });
-
